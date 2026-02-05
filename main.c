21491b601b90bd0fe661775601de1157ace564b5
nimaaskarian <nimaaskarian.work@outlook.com>
2026-02-05 17:40:18 +0330

    feat: add new level format and parser

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
+#include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 #include <ctype.h>
 #include <ncurses.h>
 
 
 int parse_times(const char ** input);
-void parse_input(const char * input);
+void parse_level(const char * input);
+
+enum Color {
+  WHITE = 1,
+  GREEN,
+  YELLOW,
+  RED,
+  BLUE,
+};
 
 int main(int argc, char *argv[])
 {
@@ -30,83 +40,42 @@ int main(int argc, char *argv[])
     fread (buffer, 1, length, fp);
   }
   fclose (fp);
-  parse_input(buffer);
+  setlocale(LC_ALL, "");
+  initscr();
+  cbreak();
+  noecho();
+  curs_set(0);
+  start_color();
+  init_pair(WHITE, COLOR_WHITE, COLOR_BLACK);
+  init_pair(GREEN, COLOR_GREEN, COLOR_BLACK);
+  init_pair(YELLOW, COLOR_YELLOW, COLOR_BLACK);
+  parse_level(buffer);
+  refresh();
+  for (;;) {}
   return EXIT_SUCCESS;
 }
 
-enum direction_t {
-  up,
-  right,
-  down,
-  left,
-};
-
-void parse_input(const char * input) {
-  enum direction_t direction = up;
+void parse_level(const char * input) {
+  int x = 0, y = 0;
   for (;*input; input++) {
     switch (*input) {
-      case 'u': 
-        direction = up;
-        puts("changed direction to up");
-        break;
-      case 'd': 
-        direction = down;
-        puts("changed direction to down");
-        break;
-      case 'r': 
-        direction = right;
-        puts("changed direction to right");
-        break;
-      case 'l': 
-        direction = left;
-        puts("changed direction to left");
-        break;
-      case 'm':
-        {
-          int n = parse_times(&input);
-          printf("%d times move with direction %d\n", n, direction);
-          // make move n times in direction
-        }
-      case 'P':
-        {
-          int n = parse_times(&input);
-          printf("%d times path with direction %d\n", n, direction);
-          // make path n times in direction
-        }
+      case 'B':
+        attron(COLOR_PAIR(GREEN));
+        mvprintw(y, x, "█");
+        x+=1;
       break;
       case 'L':
-        {
-          int n = parse_times(&input);
-          printf("%d times light with direction %d\n", n, direction);
-          // make light n times in direction
-        }
+        attron(COLOR_PAIR(YELLOW));
+        mvprintw(y, x, "█");
+        x+=1;
       break;
       case ' ':
+        x+=1;
+      break;
       case '\n':
-      case '\r':
-      case '\t':
-      continue;
-      default:
-      fprintf(stderr, "warning: invalid char %c skipped\n", *input);
+        y+=1;
+        x=0;
       break;
     }
-  }
-}
-
-int parse_times(const char ** input) {
-  int i = 1;
-  if ((*input)[i] == '*') {
-    if (!isdigit((*input)[++i])) {
-      fprintf(stderr, "error: expected number after *. found another character '%c'\n", *input[i]);
-      exit(1);
-    }
-    while (isdigit((*input)[i])) i++;
-    char * num_str = malloc(sizeof(char)*(i-1));
-    strncpy(num_str, (*input)+2, i-2);
-    num_str[i-2] = 0;
-    *input+=i;
-    return atoi(num_str);
-  } else {
-    return 1;
   }
 }
